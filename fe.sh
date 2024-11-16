5e0161a712397f6f7217d37fe6477ee137e1eabd
zjma6 <zjma6@iflytek.com>
2024-11-16 21:28:59 +0800

    update fe.sh

--- fe.sh
+++ fe.sh
@@ -3,7 +3,7 @@
 cp -f ./docker/initsql/a-n9e.sql n9e.sql
 
 if [ ! -d "./pub" ]; then
-    TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
+    TAG=$(curl -sX GET https://api.github.com/repos/caapap/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
     if ! curl -o stellar-fe-${TAG}.zip -L https://github.com/caapap/fe/releases/download/${TAG}/stellar-fe-${TAG}.zip; then
         echo "failed to download stellar-fe-${TAG}.zip!"
         exit 1
